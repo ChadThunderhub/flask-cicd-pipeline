ba74120f12d23e02c91f911645ef00271639ec39
ChadThunderhub <zoltamordemuzrob@gmail.com>
2026-02-16 23:13:59 +0100

    fix: correct FLASK_APP path in workflow and swap student index for email

--- webapp/app.py
+++ webapp/app.py
@@ -3,19 +3,19 @@
 from flask import Flask, render_template
 import os
 
-from webapp.code import FirstName, LastName, Index
-from webapp.code import FIRST_NAME, LAST_NAME, INDEX
+from webapp.code import FirstName, LastName, Mail
+from webapp.code import FIRST_NAME, LAST_NAME, MAIL
 from datetime import datetime, timezone
 
 app = Flask(__name__)
 
 @app.route('/',  methods=['GET'])
 def home():
-    return render_template('main.html', firstName=FirstName(FIRST_NAME), lastName=LastName(LAST_NAME), index=Index(INDEX))
+    return render_template('main.html', firstName=FirstName(FIRST_NAME), lastName=LastName(LAST_NAME), mail=Mail(MAIL))
 
 @app.route('/main',  methods=['GET'])
 def main():
-    return render_template('main.html', firstName=FirstName(FIRST_NAME), lastName=LastName(LAST_NAME), index=Index(INDEX))
+    return render_template('main.html', firstName=FirstName(FIRST_NAME), lastName=LastName(LAST_NAME), mail=Mail(MAIL))
 
 @app.route('/time',  methods=['GET'])
 def time():
